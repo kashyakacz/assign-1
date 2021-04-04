984323d78703c78f973aa8cf1b8c2c382777ae30
Kashya Kaczkowska <Kashya.Kacz@yahoo.ca>
2021-04-04 16:26:45 -0700

    Included a javascript file. Added some commented notes based on what functions to make the website interactive

---
+++ JS/app.js
@@ -0,0 +1,19 @@
+/* A function to add and save a new note to an array:
+- Read the text from the textarea (google how to do this)
+- string variables
+- loop it into the array of saved notes
+- display a new note on the sidebar with a number based on how many loops
+- does not return anything*/
+
+/*A function to delete a note
+- the delete button will be clicked
+- the function will search which note is currently being opened
+- will then use a for loop through the array to find the match
+- will remove the matched data
+- will update the number notes in the sidebar*/
+
+/* A function to display a note that is clicked on (google)
+- User clicks on a note
+- the program will then read the data
+- remove the original state of the webpage and show the note
+- add a close button  */
